5e3948ea6c801c0c83780d74ae9e403d31a77505
Samin Yasar <yasarsamin57@gmail.com>
2025-10-11 16:08:33 +0600

    fix: function name typo fixed

--- app/on-boarding/_layout.tsx
+++ app/on-boarding/_layout.tsx
@@ -1,18 +1,18 @@
 import { Stack } from "expo-router";
 
-export default function OnBoardingLayotu() {
-    return (
-        <>
-            <Stack.Screen
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack
-                screenOptions={{
-                    headerShown: false,
-                }}
-            />
-        </>
-    );
+export default function OnBoardingLayout() {
+	return (
+		<>
+			<Stack.Screen
+				options={{
+					headerShown: false,
+				}}
+			/>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+			/>
+		</>
+	);
 }
