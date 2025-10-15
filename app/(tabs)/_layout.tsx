0dfcba6427d10695da0d5050018f4de7535e6e90
Samin Yasar <yasarsamin57@gmail.com>
2025-10-15 18:10:11 +0600

    dev: app global bottom tab navigation added

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,57 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Pyramid, Wallet } from "lucide-react-native";
+import { useColorScheme } from "react-native";
+
+export default function TabsLayout() {
+	const colorScheme = useColorScheme();
+	const isDark = colorScheme === "dark";
+
+	return (
+		<>
+			<StatusBar
+				translucent={true}
+				backgroundColor={isDark ? "transparent" : "#000"}
+			/>
+			<Tabs
+				screenOptions={{
+					headerShown: false,
+					tabBarStyle: {
+						height: 80,
+					},
+					tabBarActiveTintColor: "#1F6E9D",
+					tabBarInactiveTintColor: "#8B8DA7",
+				}}
+			>
+				<Tabs.Screen
+					name="home"
+					options={{
+						title: "Home",
+						tabBarIcon: ({ color, size }) => (
+							<Ionicons name="home" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="activities"
+					options={{
+						title: "Activities",
+						tabBarIcon: ({ color, size }) => (
+							<Pyramid color={color} size={size} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="wallet"
+					options={{
+						title: "Wallet",
+						tabBarIcon: ({ color, size }) => (
+							<Wallet color={color} size={size} />
+						),
+					}}
+				/>
+			</Tabs>
+		</>
+	);
+}
---
+++ app/job/[id].tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button";
+import { useLocalSearchParams } from "expo-router";
+import { ArrowLeft } from "lucide-react-native";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import { jobs } from "../home";
+
+export default function JobDetails() {
+	const { id } = useLocalSearchParams();
+	const job = jobs.find((job) => job.id === Number(id));
+
+	if (!job) {
+		return (
+			<View className="flex-1 items-center justify-center">
+				<Text>Job not found</Text>
+			</View>
+		);
+	}
+
+	return (
+		<SafeAreaView className="w-full h-full">
+			<View className="w-full h-24 flex flex-row gap-3 bg-[#F3FAFC] p-5 items-center">
+				<ArrowLeft size={20} color="#636363" />
+				<Text className="text-[#515151] text-2xl font-medium">
+					Job detail
+				</Text>
+			</View>
+			<ScrollView
+				className="p-5"
+				contentContainerStyle={{
+					flexGrow: 1,
+					paddingBottom: 40,
+				}}
+				contentContainerClassName="gap-6"
+			>
+				<View className="w-full flex flex-1 flex-col gap-3">
+					<Text className="text-[#4D4D4D] text-xl font-medium">
+						{job.title}
+					</Text>
+					<Text className="text-[#808080] text-base font-normal">
+						{job.postedAt}
+					</Text>
+					<Text className="text-[#808080] text-base font-medium">
+						Job Description
+					</Text>
+
+					<Text className="text-[#999999] text-base font-medium">
+						{job.description}
+					</Text>
+					<Text className="text-[#808080] text-base font-medium">
+						Skills and expertise
+					</Text>
+
+					<Button title="Apply Now" className="mt-8" />
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
+}
