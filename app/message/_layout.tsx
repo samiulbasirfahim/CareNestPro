9af775a9d8f2bbef1dc62943a5f0f8f34b1d27df
Samin Yasar <yasarsamin57@gmail.com>
2025-10-16 12:13:38 +0600

    dev: message layout added

---
+++ app/message/_layout.tsx
@@ -0,0 +1,27 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
+
+export default function MessageLayout() {
+	const colorScheme = useColorScheme();
+	const isDark = colorScheme === "dark";
+
+	return (
+		<>
+			<StatusBar
+				translucent={true}
+				backgroundColor={isDark ? "transparent" : "#000"}
+			/>
+			<Stack.Screen
+				options={{
+					headerShown: false,
+				}}
+			/>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+			/>
+		</>
+	);
+}
