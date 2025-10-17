9513f5167852fd1cdeee4f582e92054968022af7
Samin Yasar <yasarsamin57@gmail.com>
2025-10-17 11:55:48 +0600

    chore: svg image rendering dependency updated

--- metro.config.js
+++ metro.config.js
@@ -2,19 +2,29 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 module.exports = (async () => {
-    const config = getDefaultConfig(__dirname);
-    const { transformer, resolver } = config;
+	const config = getDefaultConfig(__dirname);
+	const { transformer, resolver } = config;
 
-    config.transformer = {
-        ...transformer,
-        babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
-    };
+	config.transformer = {
+		...transformer,
+		babelTransformerPath: require.resolve(
+			"react-native-svg-transformer/expo"
+		),
+	};
 
-    config.resolver = {
-        ...resolver,
-        assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-        sourceExts: [...resolver.sourceExts, "svg"],
-    };
+	config.resolver = {
+		...resolver,
+		assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+		sourceExts: [...resolver.sourceExts, "svg"],
+	};
 
-    return withNativeWind(config, { input: "./app/global.css" });
+	config.transformer.babelTransformerPath = require.resolve(
+		"react-native-svg-transformer"
+	);
+	config.resolver.assetExts = config.resolver.assetExts.filter(
+		(ext) => ext !== "svg"
+	);
+	config.resolver.sourceExts.push("svg");
+
+	return withNativeWind(config, { input: "./app/global.css" });
 })();
