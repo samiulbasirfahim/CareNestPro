ec571bff529bc8710c0ecfaf6ee93424a1c9162a
Samin Yasar <yasarsamin57@gmail.com>
2025-10-17 11:55:22 +0600

    fix: provider/seeker onboarding index scrollview issue fixed

--- app/on-boarding/provider/provider-choose.tsx
+++ app/on-boarding/provider/provider-choose.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/components/ui/header";
 import { Typography } from "@/components/ui/typography";
 import { router } from "expo-router";
 import { useState } from "react";
-import { Pressable, SafeAreaView, View } from "react-native";
+import { Pressable, SafeAreaView, ScrollView, View } from "react-native";
 
 export default function Page() {
 	const [selectedProvider, setSelectedProvider] = useState<
@@ -17,72 +17,83 @@ export default function Page() {
 				title="What category of Care are you are experienced in"
 				subtitle="Kindly select options to help us understand your skills"
 			/>
-			<SafeView className="p-6 gap-8">
-				<View className="gap-4">
-					<View className="gap-4 flex-row">
-						<OptionCard
-							image={require("@/assets/images/on-boarding/category/image-1.png")}
-							title="Childcare"
-							subtitle="Be the right child care provider"
-							selected={selectedProvider === "childcare"}
-							onPress={() => setSelectedProvider("childcare")}
-						/>
-						<OptionCard
-							image={require("@/assets/images/on-boarding/category/image-2.png")}
-							title="Elderly Care"
-							subtitle="Be the perfect elderly care provider"
-							selected={selectedProvider === "elderly"}
-							onPress={() => setSelectedProvider("elderly")}
-						/>
+			<ScrollView
+				className="py-0 w-full bg-white"
+				contentContainerStyle={{
+					flexGrow: 1,
+					paddingBottom: 40,
+				}}
+				contentContainerClassName="gap-6"
+			>
+				<SafeView className="p-6 gap-8">
+					<View className="gap-4">
+						<View className="gap-4 flex-row">
+							<OptionCard
+								image={require("@/assets/images/on-boarding/category/image-1.png")}
+								title="Childcare"
+								subtitle="Be the right child care provider"
+								selected={selectedProvider === "childcare"}
+								onPress={() => setSelectedProvider("childcare")}
+							/>
+							<OptionCard
+								image={require("@/assets/images/on-boarding/category/image-2.png")}
+								title="Elderly Care"
+								subtitle="Be the perfect elderly care provider"
+								selected={selectedProvider === "elderly"}
+								onPress={() => setSelectedProvider("elderly")}
+							/>
+						</View>
+						<View className="gap-4 flex-row">
+							<OptionCard
+								image={require("@/assets/images/on-boarding/category/image-3.png")}
+								title="Tutoring"
+								subtitle="Provide exper tutors for "
+								selected={selectedProvider === "tutoring"}
+								onPress={() => setSelectedProvider("tutoring")}
+							/>
+							<OptionCard
+								image={require("@/assets/images/on-boarding/category/image-4.png")}
+								title="HouseKeeping"
+								subtitle="Find Reliable House Keepers"
+								selected={selectedProvider === "housekeeping"}
+								onPress={() =>
+									setSelectedProvider("housekeeping")
+								}
+							/>
+						</View>
 					</View>
-					<View className="gap-4 flex-row">
-						<OptionCard
-							image={require("@/assets/images/on-boarding/category/image-3.png")}
-							title="Tutoring"
-							subtitle="Provide exper tutors for "
-							selected={selectedProvider === "tutoring"}
-							onPress={() => setSelectedProvider("tutoring")}
-						/>
-						<OptionCard
-							image={require("@/assets/images/on-boarding/category/image-4.png")}
-							title="HouseKeeping"
-							subtitle="Find Reliable House Keepers"
-							selected={selectedProvider === "housekeeping"}
-							onPress={() => setSelectedProvider("housekeeping")}
-						/>
-					</View>
-				</View>
 
-				<Pressable
-					style={({ pressed }) => ({
-						opacity: pressed ? 0.7 : 1,
-						transform: [{ scale: pressed ? 0.98 : 1 }],
-					})}
-					className="bg-primary items-center py-3 rounded-lg w-full border-2 border-primary"
-					onPress={() => {
-						router.push({
-							pathname:
-								selectedProvider === "childcare"
-									? "/on-boarding/provider/childcare"
-									: selectedProvider === "elderly"
-										? "/on-boarding/provider/elderly"
-										: selectedProvider === "tutoring"
-											? "/on-boarding/provider/tutoring"
-											: selectedProvider ===
-												  "housekeeping"
-												? "/on-boarding/provider/housekeeping"
-												: "/splash",
-						});
-					}}
-				>
-					<Typography
-						variant="subtitle"
-						className="text-center text-lg text-white font-semibold"
+					<Pressable
+						style={({ pressed }) => ({
+							opacity: pressed ? 0.7 : 1,
+							transform: [{ scale: pressed ? 0.98 : 1 }],
+						})}
+						className="bg-primary items-center py-3 rounded-lg w-full border-2 border-primary"
+						onPress={() => {
+							router.push({
+								pathname:
+									selectedProvider === "childcare"
+										? "/on-boarding/provider/childcare"
+										: selectedProvider === "elderly"
+											? "/on-boarding/provider/elderly"
+											: selectedProvider === "tutoring"
+												? "/on-boarding/provider/tutoring"
+												: selectedProvider ===
+													  "housekeeping"
+													? "/on-boarding/provider/housekeeping"
+													: "/splash",
+							});
+						}}
 					>
-						Next
-					</Typography>
-				</Pressable>
-			</SafeView>
+						<Typography
+							variant="subtitle"
+							className="text-center text-lg text-white font-semibold"
+						>
+							Next
+						</Typography>
+					</Pressable>
+				</SafeView>
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
--- app/on-boarding/seeker/index.tsx
+++ app/on-boarding/seeker/index.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/components/ui/header";
 import { Typography } from "@/components/ui/typography";
 import { router } from "expo-router";
 import { useState } from "react";
-import { Pressable, SafeAreaView, View } from "react-native";
+import { Pressable, SafeAreaView, ScrollView, View } from "react-native";
 
 export default function Page() {
 	const [selectedProvider, setSelectedProvider] = useState<
@@ -17,72 +17,83 @@ export default function Page() {
 				title="What category of Care are you Interested in"
 				subtitle="Kindly select options to help us understand your preferences"
 			/>
-			<SafeView className="p-6 gap-8">
-				<View className="gap-4">
-					<View className="gap-4 flex-row">
-						<OptionCard
-							image={require("@/assets/images/on-boarding/category/image-1.png")}
-							title="Childcare"
-							subtitle="Find the Right child Care Provider"
-							selected={selectedProvider === "childcare"}
-							onPress={() => setSelectedProvider("childcare")}
-						/>
-						<OptionCard
-							image={require("@/assets/images/on-boarding/category/image-2.png")}
-							title="Elderly Care"
-							subtitle="Find Your Perfect Elderly Care provider"
-							selected={selectedProvider === "elderly"}
-							onPress={() => setSelectedProvider("elderly")}
-						/>
+			<ScrollView
+				className="py-0 w-full bg-white"
+				contentContainerStyle={{
+					flexGrow: 1,
+					paddingBottom: 40,
+				}}
+				contentContainerClassName="gap-6"
+			>
+				<SafeView className="p-6 gap-8">
+					<View className="gap-4">
+						<View className="gap-4 flex-row">
+							<OptionCard
+								image={require("@/assets/images/on-boarding/category/image-1.png")}
+								title="Childcare"
+								subtitle="Find the Right child Care Provider"
+								selected={selectedProvider === "childcare"}
+								onPress={() => setSelectedProvider("childcare")}
+							/>
+							<OptionCard
+								image={require("@/assets/images/on-boarding/category/image-2.png")}
+								title="Elderly Care"
+								subtitle="Find Your Perfect Elderly Care provider"
+								selected={selectedProvider === "elderly"}
+								onPress={() => setSelectedProvider("elderly")}
+							/>
+						</View>
+						<View className="gap-4 flex-row">
+							<OptionCard
+								image={require("@/assets/images/on-boarding/category/image-3.png")}
+								title="Tutoring"
+								subtitle="Find Expert Tutors for Every Subject Area"
+								selected={selectedProvider === "tutoring"}
+								onPress={() => setSelectedProvider("tutoring")}
+							/>
+							<OptionCard
+								image={require("@/assets/images/on-boarding/category/image-4.png")}
+								title="HouseKeeping"
+								subtitle="Find Reliable House Keepers"
+								selected={selectedProvider === "housekeeping"}
+								onPress={() =>
+									setSelectedProvider("housekeeping")
+								}
+							/>
+						</View>
 					</View>
-					<View className="gap-4 flex-row">
-						<OptionCard
-							image={require("@/assets/images/on-boarding/category/image-3.png")}
-							title="Tutoring"
-							subtitle="Find Expert Tutors for Every Subject Area"
-							selected={selectedProvider === "tutoring"}
-							onPress={() => setSelectedProvider("tutoring")}
-						/>
-						<OptionCard
-							image={require("@/assets/images/on-boarding/category/image-4.png")}
-							title="HouseKeeping"
-							subtitle="Find Reliable House Keepers"
-							selected={selectedProvider === "housekeeping"}
-							onPress={() => setSelectedProvider("housekeeping")}
-						/>
-					</View>
-				</View>
 
-				<Pressable
-					style={({ pressed }) => ({
-						opacity: pressed ? 0.7 : 1,
-						transform: [{ scale: pressed ? 0.98 : 1 }],
-					})}
-					className="bg-primary items-center py-3 rounded-lg w-full border-2 border-primary"
-					onPress={() => {
-						router.push({
-							pathname:
-								selectedProvider === "childcare"
-									? "/on-boarding/seeker/child-care/details-1"
-									: selectedProvider === "elderly"
-										? "/on-boarding/seeker/elderly-care/details-1"
-										: selectedProvider === "tutoring"
-											? "/on-boarding/seeker/tutoring/details-1"
-											: selectedProvider ===
-												  "housekeeping"
-												? "/on-boarding/seeker/housekeeping/details-1"
-												: "/splash",
-						});
-					}}
-				>
-					<Typography
-						variant="subtitle"
-						className="text-center text-lg text-white font-semibold"
+					<Pressable
+						style={({ pressed }) => ({
+							opacity: pressed ? 0.7 : 1,
+							transform: [{ scale: pressed ? 0.98 : 1 }],
+						})}
+						className="bg-primary items-center py-3 rounded-lg w-full border-2 border-primary"
+						onPress={() => {
+							router.push({
+								pathname:
+									selectedProvider === "childcare"
+										? "/on-boarding/seeker/child-care/details-1"
+										: selectedProvider === "elderly"
+											? "/on-boarding/seeker/elderly-care/details-1"
+											: selectedProvider === "tutoring"
+												? "/on-boarding/seeker/tutoring/details-1"
+												: selectedProvider ===
+													  "housekeeping"
+													? "/on-boarding/seeker/housekeeping/details-1"
+													: "/splash",
+							});
+						}}
 					>
-						Next
-					</Typography>
-				</Pressable>
-			</SafeView>
+						<Typography
+							variant="subtitle"
+							className="text-center text-lg text-white font-semibold"
+						>
+							Next
+						</Typography>
+					</Pressable>
+				</SafeView>
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
