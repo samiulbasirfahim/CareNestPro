2c1e4e5327a7393abc092ed5a9e7bfa51102cf03
Samin Yasar <yasarsamin57@gmail.com>
2025-11-12 15:14:50 +0600

    fix: reusable header component back button rendering is now optoinal

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -7,15 +7,21 @@ import { Typography } from "./typography";
 type Props = {
 	title: string;
 	subtitle: string;
+	showBackButton?: boolean;
 } & ViewProps;
 
-export function Header({ title, subtitle, ...props }: Props) {
+export function Header({
+	title,
+	subtitle,
+	showBackButton = true,
+	...props
+}: Props) {
 	const { top } = useSafeAreaInsets();
 
 	return (
 		<View style={{ marginTop: top }} className="p-4 gap-4 bg-white">
 			<View className="flex-row items-center gap-2">
-				<HeaderBackButton />
+				{showBackButton && <HeaderBackButton />}
 				<Typography variant="title" className="flex-1">
 					{title}
 				</Typography>
